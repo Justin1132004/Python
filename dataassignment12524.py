1f0beb383b5d411a97ed272ea47aa7e447d188f7
Justin1132004 <157507422+Justin1132004@users.noreply.github.com>
2024-01-25 10:49:07 -0600

    Assignment
    Changed the values of the example assignment

---
+++ dataassignment12524.py
@@ -0,0 +1,21 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = 5
+print("Literal:", x)
+
+# Integer
+y = -10
+print("Integer:", y)
+
+# Float
+z = 0.5
+print("Float:", z)
+
+# String
+fullname = "Justin John Morvay"
+print("String:", fullname)
+
+# Boolean
+is_active = True
+print("Boolean:", is_active)
