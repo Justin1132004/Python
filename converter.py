db56bc4bc673b2ec3f4cc2c16c2b3eef40be32ba
Justin1132004 <157507422+Justin1132004@users.noreply.github.com>
2024-01-30 11:21:41 -0600

    Converter kilograms to pounds

---
+++ converter.py
@@ -0,0 +1,30 @@
+#Convert Kilograms to Pounds
+
+
+
+#Values
+kg_value1 = 5
+kg_value2 = 25
+kg_value3 = 45
+kg_value4 = 60
+
+
+
+#Conversion
+conversion = 2.20462
+
+
+
+#Calculate each value
+lb_value1 = kg_value1 * conversion
+lb_value2 = kg_value2 * conversion
+lb_value3 = kg_value3 * conversion
+lb_value4 = kg_value4 * conversion
+
+
+
+#Calculations
+print(f"{kg_value1} kilograms is equal to {lb_value1:.2f} pounds.")
+print(f"{kg_value2} kilograms is equal to {lb_value2:.2f} pounds.")
+print(f"{kg_value3} kilograms is equal to {lb_value3:.2f} pounds.")
+print(f"{kg_value4} kilograms is equal to {lb_value4:.2f} pounds.")
