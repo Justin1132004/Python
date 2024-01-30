ea0d8593aa32b6731599de3ab1133554774be43a
Justin1132004 <157507422+Justin1132004@users.noreply.github.com>
2024-01-30 11:44:56 -0600

    Comments

---
+++ comments.py
@@ -0,0 +1,8 @@
+#How much gas are we using?
+
+tank_size = 15 #The amount of gas we have
+distance = 600 #The distance we are traveling in miles
+mpg = distance / tank_size #conversion how many miles per gallon
+print("Miles per gallon:", mpg)
+
+# 
