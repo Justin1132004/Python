8348a6ab9343c5c7bdca28b29fdac7f67da81288
Justin1132004 <157507422+Justin1132004@users.noreply.github.com>
2024-02-26 23:01:52 -0600

    Area Assignment

---
+++ areaassignment.py
@@ -0,0 +1,8 @@
+def square(side):
+    print(f"The area of the square is {side * side} square units.")
+
+def circle(radius):
+    print(f"The area of the circle is {3.14 * radius * radius} square units.")
+
+square(7)
+circle (8)
