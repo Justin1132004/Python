b94fc3e147e7b491a54b088b20524185f3ebb2a2
Justin1132004 <157507422+Justin1132004@users.noreply.github.com>
2024-02-29 11:48:00 -0600

    Exception handling assignment

--- exceptionhandling.py
+++ exceptionhandling.py
@@ -8,10 +8,10 @@ def square_number():
         squared_number = int(number) ** 2
         print(f"The square of {number} is {squared_number}.")
 
-    except ValueError:
+    except ValueError: #If user inputs an invalid character, explain value error.
 
         print("Value Error: That won't work, you need to put in a valid number.")
-        square_number()
+        square_number() #Rerun the function
     except:
         print("Something went wrong!")
 
