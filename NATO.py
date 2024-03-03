7450971dce617b95598dec7ea76ad343c58dde85
Justin1132004 <157507422+Justin1132004@users.noreply.github.com>
2024-03-03 17:57:11 -0600

    NATO Assignment

--- NATO.py
+++ NATO.py
@@ -2,7 +2,7 @@ def get_word():
     userword = input("Please enter a word: ").upper() #Ask user to input word
     nato_alphebet = {"A":"Alpha","B":"Bravo","C":"Charlie","D":"Delta","E":"Echo","F":"Foxtrot","G":"Golf","H":"Hotel","I":"India","J":"Juliette","K":"Kilo","L":"Lima","M":"Mike","N":"November","O":"Oscar","P":"Papa","Q":"Quebec","R":"Romeo","S":"Sierra","T":"Tango","U":"Uniform","V":"Victor","W":"Whiskey","X":"X-ray","Y":"Yankee","Z":"Zulu"}
     for v in userword: #Get each letter in the user inputted word
-        print(nato_alphebet.get(v)) #Get tupple's equivalent of the letter
+        print(nato_alphebet.get(v)) #Get dictionvary value equivalent of the letter
 
 def main():
 
