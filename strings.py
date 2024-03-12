197074be57e496bf8266ec24b4bd7d14227b40b7
Justin1132004 <157507422+Justin1132004@users.noreply.github.com>
2024-03-12 10:27:20 -0500

    Update strings.py
    comments

--- strings.py
+++ strings.py
@@ -1,12 +1,12 @@
 def main():
 
-    user_input = input("Enter a character: ")
+    user_input = input("Enter a character: ") #Get user inputted character
 
-    while len(user_input) != 1:
+    while len(user_input) != 1: #If user inputs more than one character or none at all, repeat the prompt and loop until they input one character.
         print("Please enter exactly one character")
         user_input = input("Enter a character: ")
 
-    ascii_value = ord(user_input)
+    ascii_value = ord(user_input) #Get the ascii value for the user inputted character
 
     print(f"The ascii value of {user_input} is {ascii_value}")
 
