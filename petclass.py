612fb9daa9d5501020d4f71e42ae2306b9dad413
Justin1132004 <157507422+Justin1132004@users.noreply.github.com>
2024-04-11 10:24:03 -0500

    Add files via upload

--- petclass.py
+++ petclass.py
@@ -1,20 +1,22 @@
 class Pet:
-
-    vet_name = "Helping Paws Veterinary"
-
-    def __init__(self,owner_first_name,owner_last_name,pet_id,pet_name,pet_type="dog"):
+#Create pet class
+    def __init__(self,owner_first_name,owner_last_name,pet_id,pet_name,pet_type="dog"): #Define pet attributes
         self.__owner_first_name = owner_first_name
         self.__owner_last_name = owner_last_name
         self.__pet_id = pet_id
         self.__pet_name = pet_name
         self.__pet_type = pet_type
+        self.__vet_name = "Helping Paws Veterinary"
 
-    def owners_firstname(self):
+    def owners_firstname(self): #Getter and Setter functions for attributes
         return self.__owner_first_name
     
     def owners_lastname(self):
         return self.__owner_last_name
     
+    def vet_name(self):
+        return self.__vet_name
+
     def pet_id(self):
         return self.__pet_id
     
@@ -25,9 +27,9 @@ class Pet:
         return self.__pet_type
 
     def display_pet_info(self):
-        return f" Owner's first name: {self.__owner_first_name}\n Owner's last name: {self.__owner_last_name}\n Pet ID: {self.__pet_id}\n Pet Name: {self.__pet_name}\n Pet Type: {self.__pet_type}"
+        return f" Owner's first name: {self.__owner_first_name}\n Owner's last name: {self.__owner_last_name}\n Pet ID: {self.__pet_id}\n Pet Name: {self.__pet_name}\n Pet Type: {self.__pet_type}\n Vet Name: {self.__vet_name}"
     
-    def check_property(self,attribute):
+    def check_property(self,attribute): #Function to check an attribute in the class
         print(hasattr(Pet,attribute))
 
 def main():
@@ -37,6 +39,6 @@ def main():
 
     print(pet1.display_pet_info())
     print(pet2.display_pet_info())
-    pet1.check_property("owners_firstname")
+    pet1.check_property("owners_firstname") #Does pet1 have an Owner's first name associated?
 
 main()
