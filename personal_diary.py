56711a8c65c09430005a421b86bf08988d482b90
Justin1132004 <157507422+Justin1132004@users.noreply.github.com>
2024-04-22 22:24:41 -0500

    Diary assignment

--- personal_diary.py
+++ personal_diary.py
@@ -1,9 +1,10 @@
 def main():
 
-
-
-
-
+    date = input("What is today's date: ")
+    diaryentry = input("What did you do today? \n")
+    record = open('diary.txt', 'a') #Open diary.txt internally so that we can modify it
+    record.write(f"{date} \n\n{diaryentry}\n\n") #Write in the lines including the user specified date and entry.
+    record.close() #Close the file internally
 
 
 main()
