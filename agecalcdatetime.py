d12d45277a5145b92a4195616ba751ceb45f434e
Justin1132004 <157507422+Justin1132004@users.noreply.github.com>
2024-04-25 10:52:35 -0500

    Update agecalcdatetime.py

--- agecalcdatetime.py
+++ agecalcdatetime.py
@@ -17,19 +17,16 @@ def main():
         birthmonth = int(input("what month were you born? (As a number from 1-12) "))
         birthday = int(input("What day of the month were you born? "))
         birthdate = datetime(birthyear, birthmonth, birthday) #Turn user inputs into date 
-        print(birthdate)
         birth = birthdate.strftime("%Y-%m-%d")
-        print(birth)
+        timedifference = today - birthdate #Difference between the inputted date and today's date
+        timedifferencedays = timedifference.days #Get days in the differenced date
+        timedifferenceyears = timedifferencedays // 365 #Divide total days by 365 to get one year
+        timedifferencemonths = timedifferencedays // 30.41 #Divide total days by 30 to get months, not 100% accurate.
+        timedifferenceweeks = timedifferencedays // 7
+        print(f"You were born on {birth}. You are {timedifferenceyears} years, {timedifferencemonths} months, {timedifferenceweeks} weeks, and {timedifferencedays} days old.")
     except Exception as e:
         print(f"Something went horribly wrong: {e}")
         main()
-
-
-
-    #birthday = datetime.strptime(birthdayinput, "%Y-%m-%d")
-    #print(birthday)
-    #print(datetime.now().time())
-    #print(datetime.now() / timedelta(birthday))
 
 
 
